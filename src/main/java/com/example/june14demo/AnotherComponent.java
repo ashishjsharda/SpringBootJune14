de97bb689d731fea20fa8fe08e0b849903e283fe
Ashish Sharda <ashishjsharda@gmail.com>
2022-06-14 05:38:55 -0500

    Add Components

--- src/main/java/com/example/june14demo/AnotherComponent.java
+++ src/main/java/com/example/june14demo/AnotherComponent.java
@@ -2,4 +2,8 @@ package com.example.june14demo;
 
 public class AnotherComponent {
     private final MyComponent myComponent;
+
+    public AnotherComponent(MyComponent myComponent) {
+        this.myComponent = myComponent;
+    }
 }
