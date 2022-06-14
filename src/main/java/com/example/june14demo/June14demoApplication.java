ef8b5a1e1620bd152650c2a00df4dcce7f1ece58
Ashish Sharda <ashishjsharda@gmail.com>
2022-06-14 05:33:33 -0500

    first commit

---
+++ src/main/java/com/example/june14demo/June14demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.june14demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class June14demoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(June14demoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/june14demo/June14demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.june14demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class June14demoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
